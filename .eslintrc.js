795e2043c9634151ae8043401a87ff3bdd746d15
Munieru <20086673+munierujp@users.noreply.github.com>
2022-10-06 02:36:21 +0900

    Develop @munierujp/eslint-config (#1)
    * Create .nvmrc
    
    * Create package.json
    
    * Create package-lock.json
    
    * Create index.js
    
    * Create .eslintrc.js
    
    * Create eslint.yml
    
    * Create release.yml
    
    * Create renovate.json
    
    * Update README

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+// @ts-check
+
+/** @typedef {import("eslint").Linter.BaseConfig } BaseConfig */
+
+/** @type {BaseConfig} */
+const config = {
+  extends: './index.js'
+}
+
+module.exports = config
